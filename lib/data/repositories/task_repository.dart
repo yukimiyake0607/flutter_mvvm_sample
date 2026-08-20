058a9e17d53016ff08095919339d3c6ab3e49a00
三宅柚希 <yukimiyake38@gmail.com>
2026-08-20 18:52:59 +0900

    feat: Add TaskRepository interface for task management
    - Introduced TaskRepository as an abstract class to define methods for task operations.
    - Methods include fetching, creating, updating, and deleting tasks, encapsulating exceptions in Result type for better error handling.

---
+++ lib/data/repositories/task_repository.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_mvvm_sample/domain/models/task.dart';
+import 'package:flutter_mvvm_sample/utils/result.dart';
+
+/// ViewModelが呼ぶabstract Repositoryです。
+/// 
+/// ViewModelには責務の観点から例外を漏らしたくないため、
+/// Repositoryでtry/catchしてResultに変換します。
+abstract class TaskRepository {
+  Future<Result<List<Task>>> getTasks();
+
+  Future<Result<Task>> getTask(String id);
+
+  Future<Result<Task>> createTask({
+    required String title,
+    required String note,
+  });
+
+  Future<Result<Task>> updateTask(Task task);
+
+  Future<Result<void>> deleteTask(String id);
+}
