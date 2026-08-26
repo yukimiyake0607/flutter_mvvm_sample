3c29deb70c844a9008050a67330147e77a88dce0
三宅柚希 <yukimiyake38@gmail.com>
2026-08-26 19:13:13 +0900

    add: routerを作成

---
+++ lib/routing/app_router.dart
@@ -0,0 +1,33 @@
+import 'package:flutter_mvvm_sample/ui/add_task/widgets/add_task_screen.dart';
+import 'package:flutter_mvvm_sample/ui/task_detail/widgets/task_detail_screen.dart';
+import 'package:flutter_mvvm_sample/ui/task_list/widgets/task_list_screen.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+final routerProvider = Provider<GoRouter>((ref) {
+  return GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) {
+          return const TaskListScreen();
+        },
+        routes: [
+          GoRoute(
+            path: 'tasks/new',
+            builder: (context, state) {
+              return AddTaskScreen();
+            },
+          ),
+          GoRoute(
+            path: 'tasks/:id',
+            builder: (context, state) {
+              final detailId = state.pathParameters['id']!;
+              return TaskDetailScreen(id: detailId);
+            },
+          ),
+        ],
+      ),
+    ],
+  );
+});
