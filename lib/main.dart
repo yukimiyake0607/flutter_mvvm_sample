6386a14386aad0d23bdb9a1135e8df18f6289204
三宅柚希 <yukimiyake38@gmail.com>
2026-08-26 19:19:37 +0900

    feat: Integrate Riverpod and update MainApp for routing
    - Wrapped MainApp in a ProviderScope to enable Riverpod state management.
    - Changed MainApp from StatelessWidget to ConsumerWidget to utilize the router configuration from Riverpod.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mvvm_sample/routing/app_router.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const ProviderScope(child: MainApp()));
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
-    );
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp.router(routerConfig: ref.watch(routerProvider));
   }
 }
