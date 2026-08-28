fda12d8bc246a251d1683660a662a46079849028
三宅柚希 <yukimiyake38@gmail.com>
2026-08-28 13:57:26 +0900

    feat: ViewModelを監視して、状態によってUIを変更する基盤作成

--- lib/ui/task_list/widgets/task_list_screen.dart
+++ lib/ui/task_list/widgets/task_list_screen.dart
@@ -1,33 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mvvm_sample/ui/task_list/view_model/task_list_view_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 
 class TaskListScreen extends ConsumerWidget {
   const TaskListScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(taskListViewModelProvider);
+    final notifier = ref.read(taskListViewModelProvider.notifier);
+
     return Scaffold(
       appBar: AppBar(title: Text('タスクリスト')),
-      body: Center(
+      body: _TaskListBody(state: state, notifier: notifier),
+    );
+  }
+}
+
+class _TaskListBody extends StatelessWidget {
+  const _TaskListBody({required this.state, required this.notifier});
+
+  final TaskListState state;
+  final TaskListViewModel notifier;
+
+  @override
+  Widget build(BuildContext context) {
+    if (state.tasks.isEmpty && state.load.running) {
+      return Center(child: const CircularProgressIndicator());
+    }
+
+    if (state.tasks.isEmpty && state.load.hasError) {
+      return Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            ElevatedButton(
-              onPressed: () {
-                context.go('/tasks/new');
-              },
-              child: Text('追加'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                context.go('/tasks/dummy');
-              },
-              child: Text('詳細へ'),
-            ),
+            Text('データの取得に失敗しました'),
+            ElevatedButton(onPressed: notifier.load, child: Text('再試行')),
           ],
         ),
-      ),
+      );
+    }
+
+    if (state.filteredTasks.isEmpty) {
+      return const Center(child: Text('タスクがありません'));
+    }
+    return ListView.builder(
+      itemCount: state.filteredTasks.length,
+      itemBuilder: (context, index) {
+        final task = state.filteredTasks[index];
+        return ListTile(
+          leading: Checkbox(value: task.isCompleted, onChanged: null),
+          title: Text(task.title),
+        );
+      },
     );
   }
 }
