07e2caf1736769c25c57bba55d81281d080237ac
三宅柚希 <yukimiyake38@gmail.com>
2026-08-28 11:54:27 +0900

    feat: TaskListScreenをConsumerWidgetに変更

--- lib/ui/task_list/widgets/task_list_screen.dart
+++ lib/ui/task_list/widgets/task_list_screen.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-class TaskListScreen extends StatelessWidget {
+class TaskListScreen extends ConsumerWidget {
   const TaskListScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(title: Text('タスクリスト')),
       body: Center(
